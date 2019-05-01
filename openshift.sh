daa67be0c666d9dd5aad51ac9d129477d948b110
chhayang <49663779+chhayang@users.noreply.github.com>
2019-05-01 11:21:38 -0400

    Create openshift.sh

---
+++ openshift.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+## login to server
+oc login -u developer -p developer 
+## --server https://172.17.0.43:8443 --insecure-skip-tls-verify
+## create project 
+
+oc new-project innovationdemo
+
+## Create App within project
+
+oc new-app https://github.com/chhayang/TestDocker.git --strategy=docker
+sleep 30s
+
+## check build logs
+
+oc logs -f bc/testdocker &&
+
+## check build status
+
+oc status
+
+## expose service
+
+oc expose service testdocker
+
+## url 
+
+oc get route
+
+##
